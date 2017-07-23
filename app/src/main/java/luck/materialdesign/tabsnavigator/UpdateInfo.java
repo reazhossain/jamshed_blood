b63de3e205b878b5473e7fc7956558320db040aa
jamshed <jamshedujjol@gmail.com>
2017-07-23 23:48:48 +0600

    facing problem in spinner

--- app/src/main/java/luck/materialdesign/tabsnavigator/UpdateInfo.java
+++ app/src/main/java/luck/materialdesign/tabsnavigator/UpdateInfo.java
@@ -2,6 +2,7 @@ package luck.materialdesign.tabsnavigator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class UpdateInfo extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class UpdateInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_info);
+
+        Spinner spinner = (Spinner) findViewById(R.id.blood_group);
+        String size = spinner.getSelectedItem().toString();
+
+        int spinner_pos = spinner.getSelectedItemPosition();
+        String[] blood_group_entries = getResources().getStringArray(R.array.blood_group_entries);
     }
 }
